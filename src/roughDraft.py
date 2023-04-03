8eff3f682131c28d4e25afcdd618b603b1277e99
Nafis-M <122175480+Nafis-M@users.noreply.github.com>
2023-04-03 01:44:14 -0400

    New changes

--- src/roughDraft.py
+++ src/roughDraft.py
@@ -3,12 +3,7 @@ import pickle, os
 
 def initialize():
     document = open("tracker.pickle","wb")
-    row1 = []
-    row2 = []
-    row3 = []
-    row4 = []
-    row5 = []
-    lists = [row1,row2,row3,row4,row5]
+    lists = []
     pickle.dump(lists,document)
     document.close()
 
@@ -21,26 +16,75 @@ def fileChecker():
         return True
 
 
-def storage(name,description,rowselection):
-    fileChecker()
+def storage(name,description, date, priority):
     
+    hold = [name,description,date,priority]
     document = open("tracker.pickle","rb")
     lists = pickle.load(document)
-    lists[rowselection].append({name:description}) 
-    print(lists[rowselection])
+    keys = len(lists) + 1
+    lists.append({keys:hold}) 
+    
     ndocument = open("tracker.pickle","wb")
     pickle.dump(lists,ndocument)
     document.close()
-    return lists[rowselection].__len__() - 1
 
+    return keys
 
+def getData(key):
+    document = open("tracker.pickle","rb")
+    lists = pickle.load(document)
 
+    value = next(d[key] for d in lists if key in d)
+    return value
 
+def getAllKeys():
+    document = open("tracker.pickle","rb")
+    lists = pickle.load(document)
+    keylist = []
+    for i in range(len(lists)):
+        keylist.append(*lists[i].keys())
+    
+    return keylist
 
-x = storage("Test2","Testin2g",3)
+def getSpecificKey(name,description, date, priority):
+    val = [name,description, date, priority]
+    document = open("tracker.pickle","rb")
+    lists = pickle.load(document)
+    for i in range(len(lists)):
+        for key, value in lists[i].items():
+            if val == value:
+                return key
+            
+    return "key doesn't exist"
 
-document = open("bugtracker.pickle","rb")
+def deleter(keys):
+    document = open("tracker.pickle","rb")
+    lists = pickle.load(document)
+    deleted = keys
+    for i in range(len(lists))
+    
+    ndocument = open("tracker.pickle","wb")
+    pickle.dump(lists,ndocument)
+    document.close()
+    
+
+initialize()
+
+storage("Test2","Testin2g","Testing", "Duh")
+storage("Tes3","Tested","Testing", "Duh")
+storage("Test4", "Testing","Testing", "Duh")
+
+print(getData(1))
+print(getAllKeys())
+print(getSpecificKey("Test2","Testin2g","Testing", "Duh"))
+document = open("tracker.pickle","rb")
 lists = pickle.load(document)
-print(lists[3],lists[0])
+print("Old List", lists)
+deleter("Test4")
+document = open("tracker.pickle","rb")
+lists = pickle.load(document)
+print("New List: ", lists)
+
+
 
 
