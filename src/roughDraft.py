c80a8aa46ae45191f7bf1188237ef464c628e700
Nafis-M <122175480+Nafis-M@users.noreply.github.com>
2023-04-03 13:30:12 -0400

    New Changes made to rough draft of the backend
    Fixed a lot of the quirks, and got rid of any bloat code lying around, added pydoc form comments to help better explain the code

--- src/roughDraft.py
+++ src/roughDraft.py
@@ -1,13 +1,15 @@
-import pickle, os
+import pickle, os, random
 
 
 def initialize():
+    ''' this program creates our dictionary and then stores it into a pickle file '''
     document = open("tracker.pickle","wb")
-    lists = {}
-    pickle.dump(lists,document)
+    storage = {}
+    pickle.dump(storage,document)
     document.close()
 
 def fileChecker():
+    ''' this function checks to see if the pickle file exists, and if not it creates it'''
     checker = os.path.isfile("tracker.pickle")
     if checker == False:
         initialize()
@@ -17,66 +19,73 @@ def fileChecker():
 
 
 def storage(name,description, date, priority):
-    
-    hold = [name,description,date,priority]
+    ''' This function stores our values from the user into a dictionary'''
+    ''' it first takes all the values submitted by the user and stores it into a List called hold '''
+    hold = [name,description,date,priority] 
     document = open("tracker.pickle","rb")
-    lists = pickle.load(document)
-    keys = len(lists) + 1
-    lists[keys] = hold
-    
+    storage = pickle.load(document)
+    ''' next it creates a "key" by taking the name submitted by the user, + a unique number '''
+    keys = name + str(random.randint(0,100)) + str(len(storage) + 1) + str(random.randint(1,9))
+    ''' storing the unique key and value into the dictionary and then storing it into the pickle '''
+    storage[keys] = hold
     ndocument = open("tracker.pickle","wb")
-    pickle.dump(lists,ndocument)
+    pickle.dump(storage,ndocument)
     document.close()
-
+    ''' returning the unique key '''
     return keys
 
-def getData(key):
+def getValue(key):
+    ''' this function returns the value of the key that was passed '''
     document = open("tracker.pickle","rb")
-    lists = pickle.load(document)
-
-    #value = next(d[key] for d in lists if key in d)
-    value = lists[key]
+    storage = pickle.load(document)
+    value = storage[key]
     return value
 
 def getAllKeys():
+    ''' this function returns ALL keys that have been stored in the pickle file '''
     document = open("tracker.pickle","rb")
-    lists = pickle.load(document)
+    storage = pickle.load(document)
     keylist = []
-    keylist = list(lists.keys())
+    keylist = list(storage.keys())
     
     return keylist
 
 def getSpecificKey(name,description, date, priority):
+    ''' this function searches for a specific key using the values in the dictionary '''
     val = [name,description, date, priority]
     document = open("tracker.pickle","rb")
-    lists = pickle.load(document)
-    key = list(lists.values()).index(val)
+    storage = pickle.load(document)
+    key = list(storage.keys())[list(storage.values()).index(val)]
+    return key
 
 def deleter(keys):
+    ''' deleting a dictionary entry using the keys '''
     document = open("tracker.pickle","rb")
-    lists = pickle.load(document)
-    lists.pop(keys)
+    storage = pickle.load(document)
+    del storage[keys]
     ndocument = open("tracker.pickle","wb")
-    pickle.dump(lists,ndocument)
+    pickle.dump(storage,ndocument)
     document.close()
     
+def editKeys(oldKey, newKey):
+    ''' This function lets you edit a key to change its name '''
+    document = open("tracker.pickle","rb")
+    storage = pickle.load(document)
+    storage[newKey] = storage[oldKey]
+    deleter(oldKey)
+    ndocument = open("tracker.pickle","wb")
+    pickle.dump(storage,ndocument)
+    document.close()
 
-initialize()
+def editValues(keys,newValue):
+    ''' This function changes the values stored in a specific key '''
+    document = open("tracker.pickle","rb")
+    storage = pickle.load(document)
+    storage[keys] = newValue
+    ndocument = open("tracker.pickle","wb")
+    pickle.dump(storage,ndocument)
+    document.close()
 
-storage("Test2","Testin2g","Testing", "Duh")
-storage("Tes3","Tested","Testing", "Duh")
-storage("Test4", "Testing","Testing", "Duh")
-
-print(getData(1))
-print(getAllKeys())
-print(getSpecificKey("Test2","Testin2g","Testing", "Duh"))
-document = open("tracker.pickle","rb")
-lists = pickle.load(document)
-print( lists)
-deleter("2")
-document = open("tracker.pickle","rb")
-lists = pickle.load(document)
-print("New List: ", lists)
 
 
 
